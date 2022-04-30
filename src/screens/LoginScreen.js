53ab41017dccb991f4ec784a2be289856e20d508
Sanskar <sskrlmsl@gmail.com>
2022-04-30 16:50:32 +0545

    Slight Changes

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -12,7 +12,7 @@ const LoginScreen = ({}) => {
       <Text style={[TextStyles.h3, { textAlign: "center" }]}>FilmFare</Text>
       <View
         style={{
-            flex:1,
+          flex: 1,
           zIndex: 10,
           marginVertical: 32,
           alignItems: "center",
@@ -25,7 +25,9 @@ const LoginScreen = ({}) => {
             borderRadius: 200,
             height: 400,
           }}
-          source={require("../../assets/backdrop.jpg")}
+          source={{
+            uri: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+          }}
           resizeMode="cover"
         />
       </View>
